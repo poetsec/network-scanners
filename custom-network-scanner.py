9dbcfa47084e978cfb5ae4cabd245e4869ef0a66
Taylor Shakespear <52114607+poetsec@users.noreply.github.com>
2021-07-29 23:49:49 -0600

    Update custom-network-scanner.py

--- custom-network-scanner.py
+++ custom-network-scanner.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Choose an IP or range to scan")
+    options = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-
-    print("IP\t\t\tMAC Address\n-----------------------------------------")
     clients_list = []
     for element in answered_list:
         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
         clients_list.append(client_dict)
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-    print(clients_list)
+    return clients_list
 
-scan("#ip address/range here")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
